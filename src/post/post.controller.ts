7575800ccf09d4c54c52475bc12cee098ea3195c
Eomhyein <mickey_8688@naver.com>
2024-07-24 20:06:24 +0900

    🩹 fix : 게시글 생성

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dtos/create-post.dto';
 import { UpdatePostDto } from './dtos/update-post.dto';
 
-@Controller('post')
+@Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
+  async create(
+    // @LogIn() user: User,
+    @Body() createPostDto: CreatePostDto
+  ) {
+    const userId = 1; // user.id;
+    const createdPost = await this.postService.create(createPostDto, userId);
+
+    return {
+      statusCode: 201,
+      message: '게시판 생성에 성공하였습니다.',
+      data: createdPost,
+    };
   }
 
-  @Get()
-  findAll() {
-    return this.postService.findAll();
-  }
+  // @Get()
+  // async findAll() {
+  //   const findAllPost = await this.postService.findAll();
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
-  }
+  //   return {
+  //     statusCode: 200,
+  //     message: '게시글 목록 조회에 성공하였습니다.',
+  //     data: findAllPost,
+  //   };
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.postService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+  //   return this.postService.update(+id, updatePostDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.postService.remove(+id);
+  // }
 }
