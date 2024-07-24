779360aaf067c684d5fdba0adb99fb838de5c99f
ttt20222 <ju83144@gmail.com>
2024-07-24 13:25:54 +0900

    add:entity setting

--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,60 @@
-export class Comment {}
+import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'comments',
+})
+export class Comment {
+    /**
+     * 코맨트 id
+     * @example 1
+     */
+    @PrimaryGeneratedColumn({ name: 'id', unsigned: true, type: 'int' })
+    id: number;
+
+    /**
+     * 부모 댓글 id
+     * @example 1
+     */
+    @IsNotEmpty()
+    @Column({ type: 'int', name: 'parent_id', unsigned: true })
+    parentId: number;
+
+    /**
+     * 게시글 id
+     * @example 1
+     */
+    @Column({ type: 'int', name: 'post_id' })
+    postId: number;
+
+    /**
+     * 사용자 id
+     * @example 1
+     */
+
+    @Column({ type: 'int', name: 'user_id' })
+    userId: number;
+
+    /**
+     * 내용
+     * @example test댓글내용
+     */
+    @IsNotEmpty()
+    @Column({ type: 'text', name: 'content' })
+    content: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
--- src/recomment/recomment.module.ts
+++ src/recomment/recomment.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RecommentService } from './recomment.service';
 import { RecommentController } from './recomment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentModule } from 'src/comment/comment.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Comment]),
+    CommentModule,
+  ],
   controllers: [RecommentController],
   providers: [RecommentService]
 })
