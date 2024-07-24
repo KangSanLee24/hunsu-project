fc58f7dfaa733a44ee5cd6f93b8c3a23599f7d76
ttt20222 <ju83144@gmail.com>
2024-07-24 15:52:22 +0900

    update:recomment service 완료(유저로직추가필요) #5

--- src/recomment/recomment.service.ts
+++ src/recomment/recomment.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRecommentDto } from './dtos/create-recomment.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -11,6 +11,22 @@ export class RecommentService {
     private CommentRepository: Repository<Comment>,
   ) {}
 
+  //대댓글 찾기
+  async findRecomment(recommentId: number) {
+
+    const findRecomment = await this.CommentRepository.findOne({
+      where: {id: recommentId}
+    });
+
+    if(!findRecomment) {
+      throw new NotFoundException(
+        '해당 대댓글이 없습니다.'
+      )
+    };
+
+    return findRecomment;
+  };
+
   //대댓글 생성
 
   async createRecomment(postId: number, commentId: number, createRecommentDto: CreateRecommentDto) {
@@ -18,18 +34,41 @@ export class RecommentService {
     const newRecomment = await this.CommentRepository.save({
       parentId: commentId,
       postId: postId,
-      userId: 1,
+      userId: 1,   //임시
       content: createRecommentDto.content
     });
     
     return newRecomment;
-  }
+  };
+
+  //대댓글 수정
 
   async updateRecomment(postId: number, commentId: number, recommentId: number, createRecommentDto: CreateRecommentDto) {
     
-  }
+    //유저 체크 로직
+
+    await this.findRecomment(recommentId);
+
+    const updateRecomment = await this.CommentRepository.update(
+      {id: recommentId},
+      {content: createRecommentDto.content}
+    );
+
+    return updateRecomment;
+  };
+
+  //대댓글 삭제
 
   async removeRecomment(postId: number, commentId: number, recommentId: number) {
+
+    //유저 체크 로직
     
-  }
+    await this.findRecomment(recommentId);
+
+    const removeRecomment = await this.CommentRepository.delete(
+      {id: recommentId},
+    );
+
+    return removeRecomment;
+  };
 }
