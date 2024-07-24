db42b8d181fb6fa5dc9326c33446c5405b4be462
이강산 <gangsan1414@gmail.com>
2024-07-24 23:11:21 +0900

    ✨ update:entity수정

--- src/dislike/entities/post-dislike.entity.ts
+++ src/dislike/entities/post-dislike.entity.ts
@@ -1,23 +1,33 @@
 import { Post } from 'src/post/entities/post.entity';
 import { User } from 'src/user/entities/user.entity';
 import {
+  Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
-@Entity('postDislikes')
+@Entity('post_dislikes')
 export class PostDislike {
   @PrimaryGeneratedColumn({ unsigned: true })
   id: number;
 
+  @Column({ type: 'int', name: 'user_id', unsigned: true })
+  userId: number;
+
+  @Column({ type: 'int', name: 'post_id', unsigned: true })
+  postId: number;
+
   @CreateDateColumn()
   createdAt: Date;
 
-  @ManyToOne(() => Post, (post) => post.postDisLikes)
+  @ManyToOne(() => Post, (post) => post.postLikes)
+  @JoinColumn({ name: 'post_id', referencedColumnName: 'id' })
   post: Post;
 
-  @ManyToOne(() => User, (user) => user.postDislikes)
+  @ManyToOne(() => User, (user) => user.postLikes)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
   user: User;
 }
