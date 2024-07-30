a7a83d8cf65b9901575f262a1072486662713886
이강산 <gangsan1414@gmail.com>
2024-07-30 20:11:02 +0900

    🔨script:babel,webpack devDependencies에 추가

---
+++ front/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/chat-list.html',
+      filename: 'chat-list.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/chat.html',
+      filename: 'chat.html',
+    }),
+  ],
+};
