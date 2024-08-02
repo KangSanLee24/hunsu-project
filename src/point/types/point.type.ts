f5c0d03da31d6f73cf131464e3a496bdf640e1ba
이강산 <gangsan1414@gmail.com>
2024-08-02 17:15:55 +0900

    🎉 add:포인트1차

--- src/point/types/point.type.ts
+++ src/point/types/point.type.ts
@@ -17,3 +17,13 @@ export enum PointScore {
   POST_LIKE = 1, // 게시글 좋아요 일일 최대 5
   COMMENT_LIKE = 1, // 댓글 좋아요 최대 10
 }
+
+export enum MaxPointScore {
+  ATTENTION = 20, // 출석 체크
+  SIGN_UP = 50, // 회원 가입
+  WEEKLY_ATTENTION = 20, // 7일 연속 출석
+  POST = 10, // 게시글 작성 일일 최대 10
+  COMMENT = 10, // 댓글 작성 일일 최대 10
+  POST_LIKE = 5, // 게시글 좋아요 일일 최대 5
+  COMMENT_LIKE = 10, // 댓글 좋아요 최대 10
+}
