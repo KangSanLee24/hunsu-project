4a0d30fcc45388c8b9c9a840c90b2073f15c0d14
이강산 <gangsan1414@gmail.com>
2024-08-06 22:12:02 +0900

    ✨ update: 1차배포0.1

--- front/config/config.js
+++ front/config/config.js
@@ -1,5 +1,8 @@
-import dotEnv from 'dotenv';
-dotEnv.config();
+// import dotEnv from 'dotenv';
+// dotEnv.config();
 
-export const API_BASE_URL = process.env.API_BASE_URL;
-export const API_LINK_URL = process.env.API_LINK_URL;
+// export const API_BASE_URL = process.env.API_BASE_URL;
+// export const API_LINK_URL = process.env.API_LINK_URL;
+
+export const API_BASE_URL = 'http://43.203.195.18:3000/api';
+export const API_LINK_URL = 'http://43.203.195.18:3000/html';
--- front/config/dotenv.js
+++ front/config/dotenv.js
@@ -1,5 +1,5 @@
-import dotEnv from 'dotenv';
-dotEnv.config();
+// import dotEnv from 'dotenv';
+// dotEnv.config();
 
-const NAVER_CLIENT_ID = process.env.NAVER_CLIENT_ID;
-const NAVER_CALLBACK_URL = process.env.NAVER_CALLBACK_URL;
+// const NAVER_CLIENT_ID = process.env.NAVER_CLIENT_ID;
+// const NAVER_CALLBACK_URL = process.env.NAVER_CALLBACK_URL;
