ba7fd99f98765f5bf8eff522383c983239a560f6
이강산 <gangsan1414@gmail.com>
2024-09-11 17:42:58 +0900

    ♻️ refactor:header.js의 refreshtoken 부분 모듈화

--- front/js/common/identify-user.js
+++ front/js/common/identify-user.js
@@ -1,5 +1,5 @@
-// 로그인 유저 정보 조회 API
-export const identifyUser = async (accessToken) => {
+/** accessToken으로 로그인 유저 정보 조회 API **/
+export const fetchAccessToken = async (accessToken) => {
   const response = await fetch(`/api/users/me`, {
     method: 'GET',
     headers: {
@@ -14,3 +14,20 @@ export const identifyUser = async (accessToken) => {
     return null;
   }
 };
+
+/** refreshToken으로 accessToken 재발급 API **/
+export const fetchRefreshToken = async (refreshToken) => {
+  const response = await fetch(`/api/auth/re-token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${refreshToken}`,
+    },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    return null;
+  }
+}
